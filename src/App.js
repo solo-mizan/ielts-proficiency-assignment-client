204c2232ea10a8cdc090ba36d9c40d711d5b5a31
solo-mizan <mizanbd.eee@gmail.com>
2022-07-25 10:46:00 +0600

    navbar and footer added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router';
+import Home from './Pages/Home/Home';
+import Dashboard from './Pages/Dashboard/Dashboard';
+import ManageProfile from './Pages/ManageProfile/ManageProfile';
+import Navbar from './Pages/Shared/Navbar';
+import Footer from './Pages/Shared/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+        <Route path='/manage-profile' element={<ManageProfile></ManageProfile>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
